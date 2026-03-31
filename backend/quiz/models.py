24c015aea773aca5b7540799690db3ed41debaa9
Trunov Vladimir <jirol9xa@gmail.com>
2026-03-31 08:07:44 +0200

    Add Event model and initial migration

---
+++ backend/quiz/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Event(models.Model):
+    month = models.PositiveSmallIntegerField(db_index=True)
+    day = models.PositiveSmallIntegerField(db_index=True)
+    year = models.IntegerField()
+    description = models.TextField()
+    pages = models.TextField(blank=True, default='')
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['month', 'day']),
+        ]
+
+    def __str__(self):
+        return f'{self.year}-{self.month:02d}-{self.day:02d}: {self.description[:60]}'
